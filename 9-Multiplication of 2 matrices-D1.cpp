277df35cfe4a3fc25caaee0d7ca915ed84c5db2a
Srujana2406 <129171029+Srujana2406@users.noreply.github.com>
2024-03-26 10:31:12 +0530

    Update 9- Time complexity Multiplication of 2 matrices-D1.cpp

--- 9-Multiplication of 2 matrices-D1.cpp
+++ 9-Multiplication of 2 matrices-D1.cpp
@@ -1,44 +1,36 @@
-#include<stdio.h>
-int main()
-{
-	int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;
-	printf("enter the number of rows and coloums:");
-	scanf("%d%d",&r,&c);
-	printf("enter the first matrix elements=\n");
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			scanf("%d",&a[i][j]);
-		}
-	}
-	printf("enter the second matrix elements=\n");
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			scanf("%d",&b[i][j]);
-		}
-	}
-	printf("multiply of the matrix=\n");
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			mul[i][j]=0;
-			for(k=0;k<c;k++)
-			{
-				mul[i][j]=a[i][k]*b[k][j];
-			}
-		}
-	}
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			printf("%d\t",mul[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
+#include <stdio.h>
+void multiply_matrices(int mat1[][3], int mat2[][2], int result[][2], int rows1, int cols1, int cols2) {
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
 }
+void print_matrix(int mat[][2], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int mat1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int mat2[3][2] = {{7, 8}, {9, 10}, {11, 12}};
+    int result[2][2];
+
+    int rows1 = 2;
+    int cols1 = 3;
+    int cols2 = 2;
+
+    multiply_matrices(mat1, mat2, result, rows1, cols1, cols2);
+
+    printf("Resultant matrix:\n");
+    print_matrix(result, rows1, cols2);
+
+    return 0;
+}
+
